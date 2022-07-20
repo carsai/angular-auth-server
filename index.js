bf24f616c56d3c3fb2bc5518c393d86b3cf86ee7
carsai <psgh1989@gmail.com>
2022-07-21 01:58:59 +0200

    Se añade Angular

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const dbConnection = require('./db/config');
@@ -20,5 +21,10 @@ app.use(express.json());
 
 // Rutas
 app.use('/api/auth', require('./routes/auth'));
+
+// Angular
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 app.listen(process.env.PORT, () => console.log(`Iniciando en el servidor ${process.env.PORT}`));
